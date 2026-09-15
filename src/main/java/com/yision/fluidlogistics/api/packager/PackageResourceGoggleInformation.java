44b39e83cd66c30b8e8ddca57a800d02f4a91d96
yision <2144287136@qq.com>
2026-09-15 18:23:54 +0800

    清理

--- src/main/java/com/yision/fluidlogistics/api/packager/PackageResourceGoggleInformation.java
+++ src/main/java/com/yision/fluidlogistics/api/packager/PackageResourceGoggleInformation.java
@@ -2,13 +2,10 @@ package com.yision.fluidlogistics.api.packager;
 
 import java.util.List;
 
-import org.jetbrains.annotations.ApiStatus;
-
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
 @FunctionalInterface
-@ApiStatus.Experimental
 public interface PackageResourceGoggleInformation {
     boolean addToGoggleTooltip(
             List<Component> tooltip,
